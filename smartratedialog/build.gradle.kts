271fab88a3100237a34ad53bb7b210f7229d5dbd
harun <harunarrosyid100@gmail.com>
2026-05-25 19:49:14 +0700

    fix error java 17 dan R dan pindah gradle dan update kalimat

--- smartratedialog/build.gradle.kts
+++ smartratedialog/build.gradle.kts
@@ -45,7 +45,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.harunarrosyid"
                 artifactId = "smartratedialog"
-                version = "1.0.5"
+                version = "1.0.6"
             }
         }
     }
